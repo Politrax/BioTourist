0f6efc3bd27ba09b9bf9f43b9ca84c64bff1b55c
RemyMach <rmachavoine@wynd.eu>
2020-04-02 10:42:35 +0200

    ajout de la possibilité de ce créer un compte seller directement

---
+++ public/js/Register/sellerInformation.js
@@ -0,0 +1,14 @@
+const sellerInformations = document.getElementById('seller_information');
+const checkButtonTourist = document.getElementById('button_tourist');
+const checkButtonSeller = document.getElementById('button_seller');
+const registerInformationTourist = document.getElementById('button_register_normal');
+
+checkButtonSeller.addEventListener('click', () => {
+    sellerInformations.style.display = 'flex';
+    registerInformationTourist.style.display = 'none';
+})
+
+checkButtonTourist.addEventListener('click', () => {
+    sellerInformations.style.display = 'none';
+    registerInformationTourist.style.display = 'flex';
+})
